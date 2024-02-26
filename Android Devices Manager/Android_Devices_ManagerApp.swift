80d159589995496683e81d2dea7fbac6703b8d1f
Georgy Dzutsev <dzgeorgy@icloud.com>
2024-02-26 12:45:38 +0300

    Initial Commit

---
+++ Android Devices Manager/Android_Devices_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Android_Devices_ManagerApp.swift
+//  Android Devices Manager
+//
+//  Created by Georgy Dzutsev on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Android_Devices_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
